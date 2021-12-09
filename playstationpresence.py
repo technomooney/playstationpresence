7b076bcfa825e2e2fc1e6fc92204d186512de9a6
marty <techno.mooney@gmail.com>
2021-12-08 20:14:29 -0600

    successfully restructured the code to use the dictionary form and got the correct icons to show up for the different platform titles. also changed the sleep time to 20 secs to be sure it does not rate limit..... we don't need to update every 15 secs....

--- playstationpresence.py
+++ playstationpresence.py
@@ -21,13 +21,9 @@ def setupLogging():
     else:
         logfile = config["system"]["POSTXLogFile"]
     loglevelint = getattr(logging, config["system"]["LogLevel"].upper(), "INFO")
-    print(loglevelint)
     logging.basicConfig(filename=logfile, level=loglevelint)
 
-
 currentOS = os.name
-
-
 config = configparser.ConfigParser()
 config.read('playstationpresence.ini')
 npsso = config['main']['npsso']
@@ -38,10 +34,11 @@ PS4OnPS5 = config['tokens']['PS4OnPS5']
 PS5 = config['tokens']['PS5']
 PS4 = config['tokens']['PS4']
 
+# todo get signaling for NT and POSTX systems to enable this program to be run as a service on both system types
 
 psnawp = psnawp.PSNAWP(npsso)
 start_time = int(time.time())
-oldpresence = ""
+oldpresence = {}
 #Initial usage, used to clear status if user is offline
 rpc = Presence(PS4,pipe=0)
 rpc.connect()
@@ -49,48 +46,52 @@ setupLogging()
 logging.info(f'Starting up at {datetime.datetime.now()}')
 while True:
     user_online_id = psnawp.user(online_id=PSNID)
-    print(type(user_online_id.get_presence()))
+    # print(type(user_online_id.get_presence()))
     mainpresence = user_online_id.get_presence()
-    print(mainpresence) #Uncomment this to get info about games inc. artwork/gameid links
+    # print(mainpresence) #Uncomment this to get info about games inc. artwork/gameid links
     start_time = int(time.time())
-    if 'offline' in mainpresence:
+    if 'offline' == mainpresence['primaryPlatformInfo']["onlineStatus"]:
         logging.info("User is offline, clearing status")
         print("User is offline, clearing status")
         rpc.clear()
-    else: 
-        if (oldpresence == mainpresence):
+    else:
+
+        if oldpresence == mainpresence:
             pass
         else:
-            #Best way to work with backwards compatability is a seprate rpclient named Playstation 5 with PS4 game assets
-            if 'PS5' in mainpresence:
-                system = "ps5"
-                if 'CUSA' in mainpresence:
-                    discordrpc(PS4OnPS5) #PS4 games on PS5
+            # Best way to work with backwards compatability is a seprate rpclient named Playstation 5 with PS4 game assets
+            if 'PS5' in mainpresence['primaryPlatformInfo']['platform'] == 'PS5':
+                system = "PS5"
+                # print(len(mainpresence))
+                if len(mainpresence) != 2:
+                    if mainpresence['gameTitleInfoList'][0]["format"].upper() == 'PS4':
+                        discordrpc(PS4OnPS5) #PS4 games on PS5
+                    else:
+                        discordrpc(PS5) #PS5 games
                 else:
-                    discordrpc(PS5) #PS5 games
+                    discordrpc(PS5)  # PS5 games
             else:
-                system = "ps4"
+                system = "PS4"
                 discordrpc(PS4)
-            current = str(mainpresence).split("'")
-            print(current)
-            if (len(current) == 19): #Length of this is 19 if user is not in a game
+
+            if len(mainpresence) == 2: #Length of this is 19 if user is not in a game
                 rpc.update(state="Idling", start=start_time, small_image=system, small_text=PSNID, large_image=system, large_text="Homescreen")
                 logging.info(f'Idleing')
                 print("Idling")
             else:
-                if 'gameStatus' in mainpresence: #Not every game supports gameStatus
-                    gametext = current[39]
+                if 'gameStatus' in mainpresence['gameTitleInfoList'][0]: #Not every game supports gameStatus
+                    gametext = mainpresence['gameTitleInfoList'][0]['gameStatus']
                 else:
-                    gametext = current[27]
+                    gametext = mainpresence['gameTitleInfoList'][0]['titleName']
                 if gameart == "yes":
                     if 'conceptIconUrl' in mainpresence['gameTitleInfoList'][0]:
                         gameid = mainpresence['gameTitleInfoList'][0]["conceptIconUrl"]
                     elif "npTitleIconUrl" in mainpresence['gameTitleInfoList'][0]:
                         gameid = mainpresence['gameTitleInfoList'][0]["npTitleIconUrl"]
-                        print()
+
                 else:
                     gameid = system
-                gamename = current[27]
+                gamename = mainpresence['gameTitleInfoList'][0]['titleName']
                 #gamestatus = current[]
                 rpc.update(state=gamename, start=start_time, small_image=system, small_text=PSNID, large_image=gameid, large_text=gametext)
                 logging.info(f"Playing {gamename}")
